b05c524b437296d1b394b85aa7ca25692e1d4bfe
Vinicius Pereira de Oliveira <vinicius.po@einstein.br>
2024-09-08 22:50:40 -0300

    get tasks

--- src/screens/home/components/task-status/index.tsx
+++ src/screens/home/components/task-status/index.tsx
@@ -1,59 +1,58 @@
 import { ScrollView, Text, View } from "react-native";
 import { Task } from "./task";
 import { TaskProps } from "./interfaces";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useAuth } from "../../../../hooks/use-auth";
 import { TASK_API } from "../../../../config/task-api";
 
+interface TaskResponse {
+  description: string;
+  id: number;
+  name: string;
+  timesExecuted: number;
+  timesToComplete: number;
+}
+
+function getTimeRecurrency(times: number): string {
+  if (times < 31) return "Mensal";
+  if (times < 61) return "Bimestral";
+  if (times < 90) return "Trimestral";
+  if (times < 120) return "Semestral";
+  return "Anual";
+}
+
+function adaptAPIResponseToModel(data: TaskResponse): TaskProps {
+  return {
+    id: data.id,
+    task: {
+      name: data.name,
+      times: {
+        completed: data.timesExecuted,
+        total: data.timesToComplete,
+      },
+    },
+    typeRecurrency: getTimeRecurrency(data.timesToComplete),
+  };
+}
+
 export function TaskStatus() {
   const { accessToken } = useAuth();
+
+  const [tasks, setTasks] = useState<TaskProps[]>([]);
+
   async function searchTasks() {
-    const tasks = await TASK_API.get("/task", {
+    const tasks = await TASK_API.get<TaskResponse[]>("/task", {
       headers: {
         Authorization: `Bearer ${accessToken}`,
       },
     });
 
-    console.log(tasks.data)
+    setTasks(tasks.data.map((task) => adaptAPIResponseToModel(task)));
   }
+
   useEffect(() => {
-    searchTasks()
+    searchTasks();
   }, []);
-  const tasks: Array<TaskProps> = [
-    {
-      id: 1,
-      typeRecurrency: "Agosto",
-      task: {
-        name: "Ler a Bíblia",
-        times: {
-          completed: 45,
-          total: 80,
-        },
-      },
-    },
-    {
-      id: 2,
-      typeRecurrency: "Agosto",
-      task: {
-        name: "Treinar",
-        times: {
-          completed: 6,
-          total: 30,
-        },
-      },
-    },
-    {
-      id: 3,
-      typeRecurrency: "Anual",
-      task: {
-        name: "Escrever Artigo",
-        times: {
-          completed: 25,
-          total: 180,
-        },
-      },
-    },
-  ];
 
   return (
     <View style={{ marginVertical: 40 }}>
